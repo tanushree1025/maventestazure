a4d89c00c41feb8c8601056f21a5a86fa91f12bb
tanu <tanushreeanand25@gmail.com>
2026-05-11 11:30:20 +0530

    first commit

---
+++ maventestazure/Javateam/src/main/java/Dineshonjava/App.java
@@ -0,0 +1,13 @@
+package Dineshonjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
